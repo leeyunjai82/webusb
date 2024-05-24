2c958b2df1dc9f3e14cec25e43a5a10333687ce5
Yunjai, Lee <leeyunjai@circul.us>
2024-05-24 12:35:42 +0900

    Create client.py

---
+++ client.py
@@ -0,0 +1,14 @@
+import serial
+
+conn = serial.Serial(port='/dev/ttyS0', baudrate=9600)
+while True:
+
+  data = ''
+  while True:
+    ch = conn.read().decode()
+    if ch == '#' or ch == '\r' or ch == '\n':
+      continue
+    if ch == '!':
+      break
+    data += ch
+  conn.write(f"#{data} - RECV!".encode('utf-8'))
